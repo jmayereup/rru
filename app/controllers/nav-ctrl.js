e4da470a052c18985f4e240e00cae0a19ccb8f7b
jmayereup <jmayereup@gmail.com>
2016-03-29 07:58:43 +0000

    fixed menu issue, added loading animation

--- app/controllers/nav-ctrl.js
+++ app/controllers/nav-ctrl.js
@@ -8,13 +8,13 @@ function NavCtrl(nav, $location) {
 
   var vm = this;
   vm.cls = nav.cls; //for menu
-  console.log("Nav1: " + vm.clsName);
+  
+  //console.log("Nav1: " + vm.clsName);
   
   vm.assignCls= function(item) {
     nav.clsName = item;
-    vm.clsName = item;
     $location.path('/class');
-    console.log("Nav2: " + nav.clsName);
+    //console.log("Nav2: " + nav.clsName);
     return item;
   };
 }
